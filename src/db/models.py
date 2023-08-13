c1ba044936556b7c1b7d68113a65f7559700720b
ray <ray.sheha@gmail.com>
2023-08-13 01:34:11 -0400

    add todos

--- src/db/models.py
+++ src/db/models.py
@@ -6,15 +6,19 @@ from src.db.database import Base
 
 class Crime(Base):
     _id = Column(String, primary_key=True)
+    # TODO add rest
 
 
 class Criminal(Base):
     _id = Column(String, primary_key=True)
+    # TODO add rest
 
 
 class Case(Base):
     _id = Column(String, primary_key=True)
+    # TODO add rest
 
 
 class Detective(Base):
     _id = Column(String, primary_key=True)
+    # TODO add rest
